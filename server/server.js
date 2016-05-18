42ff4670c8bc84f692769d77a3a5b9dc2270b147
christuttle <crunch.tuttle@gmail.com>
2016-05-18 14:46:43 -0500

    node modules stuff

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var 
+ 
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/index.html'));
