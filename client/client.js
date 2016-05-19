9f4e222a0a9fac7b03028ddebc09917d3345b636
Chris Rabuse <rab233@live.com>
2016-05-19 11:32:51 -0500

    Add client.min.js to app view and correct login route

--- client/client.js
+++ client/client.js
@@ -1,12 +1,15 @@
-var app = angular.module('tryoutApp', ['ngRoute']);
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+var app = angular.module('tryoutsApp', ['ngRoute']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $routeProvider
-  .when('/', {
-    templateUrl: 'app/partials/login',
-    controller: 'LoginController',
-    controllerAs: 'login'
-  })
+    .when('/', {
+      templateUrl: '/app/view/login',
+      controller: 'LoginController',
+      controllerAs: 'login',
+    })
+  $locationProvider.html5Mode(true);
+}]);
 
-
-
-}])
+app.controller('LoginController', function() {
+  
+})
--- server/routes/router.js
+++ server/routes/router.js
@@ -2,7 +2,7 @@ var router = require('express').Router();
 var index = require('./index');
 var partials = require('./partials');
 
-router.use('/app/partials', partials);
+router.use('/app/view', partials);
 router.use('/', index);
 
 module.exports = router;
