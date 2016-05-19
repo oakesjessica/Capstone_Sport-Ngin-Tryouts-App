56116586178ca3320100fa1f4c81ad500026fe8f
Chris Rabuse <rab233@live.com>
2016-05-19 16:37:36 -0500

    Add source maps for mobile-angular-ui to grunt file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var path = require('path');
 
-router.get('/', function(req, res){
+router.get('/*', function(req, res){
   res.render(path.join(__dirname, '../public/views/app.jade'));
 });
 
