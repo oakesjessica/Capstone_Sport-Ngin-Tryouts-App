7681cff0c0d972f0518f50a14c59d0f275e95c76
christuttle <crunch.tuttle@gmail.com>
2016-05-19 09:23:09 -0500

    fixes file structure so localhost works

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
- 
+
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
 })
 
 var server = app.listen(process.env.PORT || 3000, function(){
