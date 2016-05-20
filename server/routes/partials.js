f4b5d39b32a13dbda8e7f9bcbfdf2872e98aba30
Chris Rabuse <rab233@live.com>
2016-05-20 09:37:37 -0500

    Add brand to navbar and swipe gesture for the slide menu

--- server/routes/partials.js
+++ server/routes/partials.js
@@ -3,5 +3,10 @@ var path = require('path');
 
 router.get('/login', function(req, res){
   res.render(path.join(__dirname, '../public/views/partials/login.jade'));
-})
+});
+
+router.get('/manage', function(req, res) {
+  res.render(path.join(__dirname, '../public/views/partials/management.jade'));
+});
+
 module.exports = router;
