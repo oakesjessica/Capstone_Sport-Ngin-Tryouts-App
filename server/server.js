349cffd11dffd23556fd6ff0f844fc6da4093173
christuttle <crunch.tuttle@gmail.com>
2016-05-20 09:38:16 -0500

    connects to db

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ var path = require('path');
 var app = express();
 var router = require('./routes/router');
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+//Database
+var mongoUri = 'mongodb://localhost/sportNginDB';
+var mongoDB = mongoose.connect(mongoUri).connection;
+mongoDB.on('error', function(err){
+  console.log('MongoDB connection error', err);
+});
+mongoDB.once('open', function(){
+  console.log('MongoDB connection open.');
+});
 //config
 app.use(morgan('dev'));
 app.use(express.static('server/public'));
