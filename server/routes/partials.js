43ec708f68388658748501f68022c8e5b702a4c9
Jessica Oakes <jessica.yoonah@gmail.com>
2016-05-24 13:19:23 -0500

    Adds more new tryout functionality, post route workign

--- server/routes/partials.js
+++ server/routes/partials.js
@@ -5,7 +5,7 @@
 var router = require('express').Router();
 var path = require('path');
 var jade = require('jade');
-var AccessCode = require('../../models/tryout');
+var Tryout = require('../../models/tryout');
 
 router.get('/', function(req, res){
   if(req.isAuthenticated()){
@@ -16,9 +16,15 @@ router.get('/', function(req, res){
 });
 
 
-router.get('/information', function(req, res) {
-  res.render(path.join(__dirname, '../public/views/partials/tryoutInformation.jade'));
+router.get('/new', function(req, res) {
+  res.render(path.join(__dirname, '../public/views/partials/newTryout.jade'));
 });
+
+router.post('/new', function(req, res) {
+  console.log(req.body, req.user.id);
+  
+});
+
 router.get('/archives', function(req, res){
   res.render(path.join(__dirname, '../public/views/partials/archives.jade'));
 });
