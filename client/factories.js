835765a82e16b22d813d36a482d06cd3dd98d454
Chris Rabuse <rab233@live.com>
2016-06-06 13:36:53 -0500

    Add auto select for guest code and fix alignment on logout link on desktop

--- client/factories.js
+++ client/factories.js
@@ -218,3 +218,17 @@ app.factory('TryoutService', ['$http', '$location', 'UserService', function($htt
     data: data
   };
 }]);
+
+app.directive('selectOnClick', ['$window', function ($window) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            element.on('click', function () {
+                if (!$window.getSelection().toString()) {
+                    // Required for mobile Safari
+                    this.setSelectionRange(0, this.value.length)
+                }
+            });
+        }
+    };
+}]);
